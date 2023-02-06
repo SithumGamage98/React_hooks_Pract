c721f2004f3b5a7ee35bfbd8a6b714e0bbb20887
Sithum Sankalpa Gamage <100986253+SithumGamage98@users.noreply.github.com>
2023-02-06 15:39:24 +0530

    Update Counter_and_docTitle.js

--- src/components_useEffect/Counter_and_docTitle.js
+++ src/components_useEffect/Counter_and_docTitle.js
@@ -4,9 +4,7 @@ export default function Counter_and_docTitle() {
 
     //useState for count value
     const [count,setCount] = useState(0)
-
     //useEffect for changeing the doc tiltle
-
     useEffect(()=>{
 
         console.log('Clicked useEffect')
@@ -14,9 +12,7 @@ export default function Counter_and_docTitle() {
     })
   return (
     <div>
-
     <button onClick={()=>setCount(count+1)}>Count : {count}</button>
-
     </div>
   )
 }
